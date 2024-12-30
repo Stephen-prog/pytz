6f7f2db58707248257e800d63b93554e623b34a5
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2024-12-30 20:49:56 +0100

    second commit

--- app.py
+++ app.py
@@ -7,10 +7,10 @@
 # print("The area of the rectangle is " + area)
 
 # Exercise 2 Shopping Cart Program
-item = input("What would you like to purchase?: ")
-price = float(input("What is the price?: "))
-quantity = int(input("How many would you like?: "))
-total = price * quantity
+# item = input("What would you like to purchase?: ")
+# price = float(input("What is the price?: "))
+# quantity = int(input("How many would you like?: "))
+# total = price * quantity
 
-print(f"You have bought {quantity} x {item}/s")
-print(f"Your total is: €{total}")
+# print(f"You have bought {quantity} x {item}/s")
+# print(f"Your total is: €{total}")
---
+++ madlib.py
@@ -0,0 +1,14 @@
+# Madlibs game
+# word game where you create a story
+# by filling in the blanks with random words
+
+adj1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (food item): ")
+adj2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb (preferably in past tense -ed): ")
+adj3 = input("Enter an adjective (description): ")
+
+print(f"Today I went to an {adj1} restaurant.")
+print(f"There was alot of {noun1} in their Menu.")
+print(f"The {noun1} looked {adj2} and well {verb1}.")
+print(f"It was {adj3}!")
