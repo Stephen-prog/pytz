850b03d307ca5de7f90698f1db51eda5c02379f4
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2024-12-30 17:01:21 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+# Exercise 1 Rectangle Area Calc
+
+# length = int(input("Enter the length: "))
+# width = int(input("Enter the width: "))
+# area = str(length * width)
+
+# print("The area of the rectangle is " + area)
+
+# Exercise 2 Shopping Cart Program
+item = input("What would you like to purchase?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
+total = price * quantity
+
+print(f"You have bought {quantity} x {item}/s")
+print(f"Your total is: €{total}")
