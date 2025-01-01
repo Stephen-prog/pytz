fa0986d15717ded017e8b86b43aedfd1af869328
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-01-01 11:55:17 +0100

    random commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# import math
 # Exercise 1 Rectangle Area Calc
 
 # length = int(input("Enter the length: "))
@@ -14,3 +15,21 @@
 
 # print(f"You have bought {quantity} x {item}/s")
 # print(f"Your total is: €{total}")
+
+# Circumference of a circle
+# radius = float(input("Enter the radius of the circle: "))
+# circumference = 2 * math.pi * radius
+# print(f"The circumference is: {round(circumference, 2)}cm")
+
+# Area of a circle
+# radius = float(input("Enter the radius of a circle: "))
+# area = math.pi * pow(radius, 2)
+# print(f"The area is: {round(area, 2)}cm²")
+
+# Pythagoras theorem
+# a = float(input("Enter side A: "))
+# b = float(input("Enter side B: "))
+
+# c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+# print(f"Side C = {c}")
