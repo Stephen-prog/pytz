c0c2446dcbc15fa6894e72232ea97043b2a93716
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-01-05 17:46:40 +0100

    mod

--- app.py
+++ app.py
@@ -179,11 +179,14 @@
 #         print(item, end=" ")
 #     print()
 
-num_pad = ((1, 2, 3),
-           (4, 5, 6),
-           (7, 8, 9),
-           ("*", 0, "#"))
-for row in num_pad:
-    for num in row:
-        print(num, end=" ")
-    print()
+# num_pad = ((1, 2, 3),
+#            (4, 5, 6),
+#            (7, 8, 9),
+#            ("*", 0, "#"))
+# for row in num_pad:
+#     for num in row:
+#         print(num, end=" ")
+#    print()
+
+# dictionary = a collection of {key:value} pairs
+#              ordered and changeable. No duplicates
