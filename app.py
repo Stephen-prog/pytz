5b90c1cd62bea36c9fbdde59f8f47777dec68245
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-01-09 16:20:29 +0100

    mod

--- app.py
+++ app.py
@@ -274,3 +274,23 @@
 #                     default is used when that argument is omitted
 #                     make your functions more flexible, reduces number of arguments
 #                     1. positional, 2. DEFAULT, 3. keyword, 4. arbitrary
+
+# def net_price(list_price, discount = 0, tax = 0.05):
+#     return list_price * (1 - discount) * (1 + tax)
+# print(net_price(500))
+# print(net_price(500, 0.1))
+# print(net_price(500,0.1, 0))
+
+# timer
+# import time
+# def count(end, start = 0):
+#     for x in range(start, end+1):
+#        print(x)
+#        time.sleep(1)
+#     print("DONE!")
+# count(5)
+
+# keyword arguments = an argument preceded by an identifier
+#                     helps withs readability
+#                     order of arguments doesn't matter
+#                     1. positional, 2. DEFAULT, 3. keyword, 4. arbitrary
