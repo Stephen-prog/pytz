1dc18cda449737a792b5b5b3cbd2f89e0275e6c8
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-01-14 18:37:30 +0100

    mod

--- app.py
+++ app.py
@@ -386,6 +386,22 @@
 #                      Compact and easier to read than traditional loops
 #                      [expression for value in iterable if condition]
 
-doubles = [x * 2 for x in range(1, 11)]
+# doubles = [x * 2 for x in range(1, 11)]
+# triples = [y * 3 for y in range(1, 11)]
+# squares = [z * z for z in range(1, 11)]
+# print(squares)
 
-print(doubles)
+# fruits = ["blueberry", "grape", "banana", "kiwi"]
+# fruits = [fruit.upper() for fruit in fruits]
+# print(fruits)
+
+# numbers = [1, -2, 3, -4, 5, -6, -7, 8]
+# position_num = [num for num in numbers if num >= 0]
+# negative_num = [num for num in numbers if num <= 0]
+# even_num = [num for num in numbers if num % 2 == 0]
+# odd_num = [num for num in numbers if num % 2 == 1]
+# print(odd_num)
+
+# grades = [97, 79, 88, 67, 49, 56]
+# passing_grades = [grade for grade in grades if grade >= 60]
+# print(passing_grades)
