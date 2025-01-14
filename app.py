eea309485649ce908fa57cfff78348f7b5db123e
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-01-14 19:07:27 +0100

    modified

--- app.py
+++ app.py
@@ -405,3 +405,45 @@
 # grades = [97, 79, 88, 67, 49, 56]
 # passing_grades = [grade for grade in grades if grade >= 60]
 # print(passing_grades)
+
+
+# Match-case statement (switch): An alternative to using 'elif' statements
+#                                Execute some code if a value matches a 'case'
+#                                Benefits: cleaner and syntax is more readable
+
+# def day_of_week(day):
+#     match day:
+#         case 1:
+#             return "It is Sunday"
+#         case 2:
+#             return "It is Monday"
+#         case 3:
+#             return "It is Tuesday"
+#         case 4:
+#             return "It is Wednesday"
+#         case 5:
+#             return "It is Thursday"
+#         case 6:
+#             return "It is Friday"
+#         case 7:
+#             return "It is Saturday"
+#         case _:
+#             return "Not valid"
+# print(day_of_week(3))
+
+# def is_weekend(day):
+#     match day:
+#         case "Saturday" | "Sunday":
+#             return True
+#         case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday":
+#             return False
+#         case _:
+#             return False
+# print(is_weekend("Sunday"))
+
+
+# module = a file containing code you want to include in your program
+#          use 'import' to include a module (built-in or your own)
+#          useful to break up a large program reusable separate files
+
+
