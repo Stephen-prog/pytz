4485bf7de345b6488d77fe8d3c0b2179871fc355
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-01-16 10:40:22 +0100

    encryption mit py

---
+++ encrypt.py
@@ -0,0 +1,11 @@
+# Encryption with Python
+
+import random
+import string
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars  = list(chars)
+key = chars.copy()
+
+print(f"chars: {chars}")
+print(f"key:   {key}")
