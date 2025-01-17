bb880dc8a8bcb118401f74b210b7bc9a0ea2d9d0
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-01-17 08:59:43 +0100

    hangman mit python

--- hangman.py
+++ hangman.py
@@ -10,10 +10,33 @@ hangman_art = {0: ("   ",
                1: (" o ",
                    "   ",
                    "   "),
-               2: ("   ",
-                   "   ",
+               2: (" o ",
+                   " | ",
                    "   "),
-               3: (),
-               4: (),
-               5: (),
-               6: ()}
+               3: (" o ",
+                   "/| ",
+                   "   "),
+               4: (" o ",
+                   "/|\\",
+                   "   "),
+               5: (" o ",
+                   "/|\\ ",
+                   "/  "),
+               6: (" o ",
+                   "/|\\",
+                   "/ \\")}
+
+def display_mane( wrong_guesses):
+    pass
+
+def display_hint(hint):
+    pass
+
+def display_answer(answer):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
