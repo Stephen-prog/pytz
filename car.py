f495fa05fcedcab5d88f26a07d3f6ee00b4e0e16
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-01-21 20:22:51 +0100

    car class

--- car.py
+++ car.py
@@ -5,3 +5,15 @@ class Car:
         self.year = year
         self.color = color
         self.for_sale = for_sale
+
+    def drive(self):
+        print(f"You are driving the {self.make} {self.model}.")
+
+    def stop(self):
+        print(f"You stopped driving the {self.make} {self.model}.")
+
+    def buy(self):
+        print(f"You want to buy {self.color} {self.make} {self.model}")
+
+    def info(self):
+        print(f"This is a {self.year} {self.make} {self.model}")
