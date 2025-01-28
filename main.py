e42d0a8c2499456257a29d5590daf1e3cedbf008
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-01-28 16:22:49 +0100

    duck typing

--- main.py
+++ main.py
@@ -6,42 +6,59 @@
 #                1. Inheritance = An object could be treated of the same type as a parent class
 #                2. "Duck typing" =Object must have necessary attributes/methods
 
-from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
 
-class Shape:
+# class Shape:
 
-    @abstractmethod
-    def area(self):
-        pass
+#     @abstractmethod
+#     def area(self):
+#         pass
 
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
+# class Circle(Shape):
+#     def __init__(self, radius):
+#         self.radius = radius
 
-    def area(self):
-        return 3.14 * self.radius ** 2
+#     def area(self):
+#         return 3.14 * self.radius ** 2
 
-class Square(Shape):
-    def __init__(self, side):
-        self.side = side
+# class Square(Shape):
+#     def __init__(self, side):
+#         self.side = side
 
-    def area(self):
-        return self.side ** 2
+#     def area(self):
+#         return self.side ** 2
 
-class Triangle(Shape):
-    def __init__(self, base, height):
-        self.base = base
-        self.height = height
+# class Triangle(Shape):
+#     def __init__(self, base, height):
+#         self.base = base
+#         self.height = height
 
-    def area(self):
-        return 0.5 * self.base * self.height
+#     def area(self):
+#         return 0.5 * self.base * self.height
 
-class Pizza(Circle):
-    def __init__(self, topping, radius):
-        super().__init__(radius)
-        self.topping = topping
+# class Pizza(Circle):
+#     def __init__(self, topping, radius):
+#         super().__init__(radius)
+#         self.topping = topping
 
-shapes = [Circle(5), Square(7), Triangle(9, 11), Pizza("pepperoni", 13) ]
+# shapes = [Circle(5), Square(7), Triangle(9, 11), Pizza("pepperoni", 13) ]
 
-for shape in shapes:
-    print(f"The area is {shape.area()}cm^2")
+# for shape in shapes:
+#     print(f"The area is {shape.area()}cm^2")
+
+
+
+# Duck-typing = Another way to achieve polymorphism besides Inheritance
+#               Object must have the minimum necessary attributes/methods
+#               "If it looks like a duck and quacks like a duck, it must be a duck."
+
+class Animal:
+    is_mammal = True
+
+class Dog(Animal):
+    def speak(self):
+        print("WOOF")
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW")
