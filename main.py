00cffcec6d3fb5e2980bd780c747e58bedb9ed77
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-01-29 19:01:34 +0100

    magic methods

--- main.py
+++ main.py
@@ -144,3 +144,75 @@ student3 = Student("Raphael", 3.8)
 print(Student.get_count())
 print(Student.get_average_gpa())
 """
+
+# Magic methods = Dunder methods (double underscore) __init__, __str__, __eq__
+#                 They are automatically called by many of Python's built-in operations.
+#                 They allow developers to define or customize the behaviour of objects
+
+"""
+class Student:
+
+    def __init__(self, name, gpa):
+        self.name = name
+        self.gpa = gpa
+
+    def __str__(self):
+        return f"name: {self.name} gpa: {self.gpa}"
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __gt__(self, other):
+        return self.gpa > other.gpa
+
+student1 = Student("Danny", 3.2)
+student2 = Student("Ghost", 3.5)
+
+print(student1)
+print(student1 == student2)
+print(student1 > student2)
+"""
+
+class Book:
+
+    def __init__(self, title, author, num_pages):
+        self.title = title
+        self.author = author
+        self.num_pages = num_pages
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
+
+    def __eq__(self, other):
+        return self.title == other.title and self.author == other.author
+
+    def __lt__(self, other):
+        return self.num_pages < other.num_pages
+
+    def __gt__(self, other):
+        return self.num_pages > other.num_pages
+
+    def __add__(self, other):
+        return f"{self.num_pages + other.num_pages} pages"
+
+    def __contains__(self, keyword):
+        return keyword in self.title or keyword in self.author
+
+    def __getitem__(self, key):
+        if key == "title":
+            return self.title
+        elif key == "author":
+            return self.author
+        elif key == "num_pages":
+            return self.num_pages
+        else:
+            return f"Key '{key}' not found"
+
+book1 = Book("The Lord of The Rings", "J.R.R. Tolkien", 1077)
+book2 = Book("Othello", "William Shakespeare", 114)
+book3 = Book("Dune Messiah", "Frank Herbert", 352)
+
+print(f"{book1['num_pages']} pages")
+print(book3['publisher'])
+
+
