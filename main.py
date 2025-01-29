71a89783406f0b34a11eb77a7ae516749105b93e
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-01-29 10:31:42 +0100

    static methods

--- main.py
+++ main.py
@@ -82,3 +82,27 @@
 
 # Instance methods = Best for operations on instances of the class (objects)
 # Static methods = Best for utility functions that do not need access to class data
+
+class Employee:
+
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+    # Instance method
+    def get_info(self):
+        return f"{self.name} = {self.position}"
+
+    @staticmethod
+    def is_valid_positions(position):
+        valid_positions = ["Manager", "Cashier", "Cook", "Waiter", "Janitor"]
+        return position in valid_positions
+
+employee1 = Employee("Danny", "Manger")
+employee2 = Employee("Sandra", "Cashier")
+employee3 = Employee("Deborah", "Cook")
+
+print(Employee.is_valid_positions("Waiter"))
+print(employee1.get_info())
+print(employee2.get_info())
+print(employee3.get_info())
