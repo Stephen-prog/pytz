7253878d26d5d9c7bf52c7739eb8ef09d6a81868
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-02-08 15:44:54 +0100

    multithreading in py

--- main.py
+++ main.py
@@ -401,3 +401,8 @@ if target_datetime < current_datetime:
 else:
     print("Target date has NOT passed")
 """
+
+
+# multithreading = Used to perform multiple tasks concurrently (multitasking)
+#                  Good for I/O bound tasks like reading files or fetching data from APIs
+#                  threading.Thread(target=my_function)
