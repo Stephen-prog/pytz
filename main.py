15ef47c6703b71f37c327efdbd9c43e48fef0e7a
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-02-10 16:07:22 +0100

    connecting to apis in py

--- main.py
+++ main.py
@@ -407,6 +407,7 @@ else:
 #                  Good for I/O bound tasks like reading files or fetching data from APIs
 #                  threading.Thread(target=my_function)
 
+"""
 import threading
 import time
 
@@ -435,3 +436,29 @@ chore2.join()
 chore3.join()
 
 print("All chores have been completed")
+"""
+
+
+# How to connect ta an API using python
+import requests
+
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(name):
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+
+pokemon_name = "charmander"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    print(f"Name: {pokemon_info["name"].capitalize()}")
+    print(f"ID: {pokemon_info["id"]}")
+    print(f"Height: {pokemon_info["height"]}")
+    print(f"Weight: {pokemon_info["weight"]}")
