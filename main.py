66c0ff0d8d8f2a1f140402a888fc8dcea31e74d3
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-02-10 23:13:50 +0100

    pyqt5 introduction

--- main.py
+++ main.py
@@ -440,6 +440,8 @@ print("All chores have been completed")
 
 
 # How to connect ta an API using python
+
+"""
 import requests
 
 base_url = "https://pokeapi.co/api/v2/"
@@ -462,3 +464,27 @@ if pokemon_info:
     print(f"ID: {pokemon_info["id"]}")
     print(f"Height: {pokemon_info["height"]}")
     print(f"Weight: {pokemon_info["weight"]}")
+"""
+
+
+
+# PyQt5 introduction
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My first GUI")
+        self.setGeometry(700, 300, 500,500)
+        self.setWindowIcon(QIcon("my_u.jpg"))
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
