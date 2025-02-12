6ead163f9aea546e6c9df13b2b64b78e92077d8e
Uzondu Stephen <74657583+Stephen-prog@users.noreply.github.com>
2025-02-12 12:19:42 +0100

    pyqt5 radio buttons

--- main.py
+++ main.py
@@ -605,6 +605,7 @@ if __name__ == "__main__":
 
 
 # PyQt5 buttons
+"""
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 
@@ -632,3 +633,98 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+"""
+
+
+
+# PyQt5 Checkboxes
+"""
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QCheckBox
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+        self.checkbox = QCheckBox("Do you like food?", self)
+        self.initUI()
+
+    def initUI(self):
+        self.checkbox.setGeometry(10, 0, 500, 100)
+        self.checkbox.setStyleSheet("font-size: 30px;"
+                                    "font-family: Arial;")
+        self.checkbox.setChecked(False)
+        self.checkbox.stateChanged.connect(self.checkbox_changed)
+
+    def checkbox_changed(self, state):
+        if state == Qt.Checked:
+            print("You like food.")
+        else:
+            print("You DON'T like food.")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+"""
+
+
+
+# PyQt5 radio buttons
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QRadioButton, QButtonGroup
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+        self.radio1 = QRadioButton("Visa", self)
+        self.radio2 = QRadioButton("Mastercard", self)
+        self.radio3 = QRadioButton("Gift Card", self)
+        self.radio4 = QRadioButton("In-Store", self)
+        self.radio5 = QRadioButton("Online", self)
+        self.button_group1 = QButtonGroup(self)
+        self.button_group2 = QButtonGroup(self)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(700, 300, 500, 500)
+        self.radio1.setGeometry(0, 0, 300, 50)
+        self.radio2.setGeometry(0, 50, 300, 50)
+        self.radio3.setGeometry(0, 100, 300, 50)
+        self.radio4.setGeometry(0, 150, 300, 50)
+        self.radio5.setGeometry(0, 200, 300, 50)
+
+        self.setStyleSheet("QRadioButton{"
+                           "font-size: 30px;"
+                           "font-family: Arial;"
+                           "padding: 10px;"
+                           "}")
+
+        self.button_group1.addButton(self.radio1)
+        self.button_group1.addButton(self.radio2)
+        self.button_group1.addButton(self.radio3)
+        self.button_group2.addButton(self.radio4)
+        self.button_group2.addButton(self.radio5)
+
+        self.radio1.toggled.connect(self.radio_button_changed)
+        self.radio2.toggled.connect(self.radio_button_changed)
+        self.radio3.toggled.connect(self.radio_button_changed)
+        self.radio4.toggled.connect(self.radio_button_changed)
+        self.radio5.toggled.connect(self.radio_button_changed)
+
+    def radio_button_changed(self):
+        radio_button = self.sender()
+        if radio_button.isChecked():
+            print(f"{radio_button.text()} has been selected.")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+
